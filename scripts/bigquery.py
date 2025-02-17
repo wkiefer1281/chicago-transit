b41b1a1195eb7d1e107614573c6a55a861f256db
Walter Kiefer <157405358+wkiefer1281@users.noreply.github.com>
2025-02-17 17:59:03 -0600

    cta functions script

--- scripts/bigquery.py
+++ scripts/bigquery.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     cta_client = CTAClient()
     df = train_locations(cta_client=cta_client)
 
-# push results to BQ
-project_id = "eternal-outlook-451201-d1"
-pandas_gbq.to_gbq(df, "chicago_transit.train_locations", project_id=project_id, if_exists="replace")
+    # push results to BQ
+    project_id = "eternal-outlook-451201-d1"
+    pandas_gbq.to_gbq(df, "chicago_transit.train_locations", project_id=project_id, if_exists="replace")
